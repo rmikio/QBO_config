3ddd322d13ac94d70ededc0382795022993f60ad
TheCorpora <indiegogo@thecorpora.com>
2018-03-08 18:14:34 +0100

    Add files via upload

--- deamonsScripts/autoStart.py
+++ deamonsScripts/autoStart.py
@@ -12,21 +12,34 @@ import subprocess
 config = yaml.safe_load(open("/home/pi/Documents/config.yml"))
 print "CONFIG " + str(config)
 
-text = "Hello"
-speak = "espeak -ven+f3 \"" + text + "\" --stdout  | aplay -D convertQBO"
+
+if (config["language"] == "spanish"):
+	speak = "pico2wave -l \"es-ES\" -w /home/pi/Documents/pico2wave.wav Hola && aplay -D convertQBO /home/pi/Documents/pico2wave.wav"
+else:
+        speak = "pico2wave -l \"en-US\" -w /home/pi/Documents/pico2wave.wav Hello && aplay -D convertQBO /home/pi/Documents/pico2wave.wav"
 result = subprocess.call(speak, shell = True)
 time.sleep(0.5)
 
-if config["startWith"] == "Scratch":
-	text = "I'm in scratch mode."
-        speak = "espeak -s 140 -ven+f3 \"" + text + "\" --stdout  | aplay -D convertQBO"
+if config["startWith"] == "scratch":
+	if (config["language"] == "spanish"):
+		text = "estoy en modo scratch."
+        	speak = "pico2wave -l \"es-ES\" -w /home/pi/Documents/pico2wave.wav \"" + text + "\" && aplay -D convertQBO /home/pi/Documents/pico2wave.wav"
+        else:
+        	text = "I'm in scratch mode."
+		speak = "pico2wave -l \"en-US\" -w /home/pi/Documents/pico2wave.wav \"" + text + "\" && aplay -D convertQBO /home/pi/Documents/pico2wave.wav"
+
         result = subprocess.call(speak, shell = True)
 
 	result = subprocess.call("/home/pi/Documents/deamonsScripts/QBO_scratch start > /home/pi/scratchMode.log 2>&1", shell = True)
 
-elif config["startWith"] == "Interactive":
-	text = "I'm in interactive mode."
-        speak = "espeak -s 140 -ven+f3 \"" + text + "\" --stdout  | aplay -D convertQBO"
+elif config["startWith"] == "interactive":
+        if (config["language"] == "spanish"):
+                text = "estoy en modo interactivo. Un momento, por favor."
+                speak = "pico2wave -l \"es-ES\" -w /home/pi/Documents/pico2wave.wav \"" + text + "\" && aplay -D convertQBO /home/pi/Documents/pico2wave.wav"
+        else:
+                text = "I'm in interactive mode. Please wait."
+                speak = "pico2wave -l \"en-US\" -w /home/pi/Documents/pico2wave.wav \"" + text + "\" && aplay -D convertQBO /home/pi/Documents/pico2wave.wav"
+
         result = subprocess.call(speak, shell = True)
 
 	result = subprocess.call("/home/pi/Documents/Python\ projects/PiFaceFast.py > /home/pi/PiFaceFastMode.log 2>&1 &", shell = True)
