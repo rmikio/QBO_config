d130827eb39033ea89035ebeab250e10147c22e5
TheCorpora <indiegogo@thecorpora.com>
2018-03-09 19:12:40 +0100

    Add files via upload

--- deamonsScripts/autoStart.py
+++ deamonsScripts/autoStart.py
@@ -13,10 +13,13 @@ config = yaml.safe_load(open("/home/pi/Documents/config.yml"))
 print "CONFIG " + str(config)
 
 
+
 if (config["language"] == "spanish"):
-	speak = "pico2wave -l \"es-ES\" -w /home/pi/Documents/pico2wave.wav <volume level='" + str(config["volume"]) + "'>" + "Hola && aplay -D convertQBO /home/pi/Documents/pico2wave.wav"
+	text = "Hola"
+	speak = "pico2wave -l \"es-ES\" -w /home/pi/Documents/pico2wave.wav \"<volume level='" + str(config["volume"]) + "'>" + text + "\" && aplay -D convertQBO /home/pi/Documents/pico2wave.wav"
 else:
-        speak = "pico2wave -l \"en-US\" -w /home/pi/Documents/pico2wave.wav <volume level='" + str(config["volume"]) + "'>" + "Hello && aplay -D convertQBO /home/pi/Documents/pico2wave.wav"
+	text = "Hello"
+        speak = "pico2wave -l \"en-US\" -w /home/pi/Documents/pico2wave.wav \"<volume level='" + str(config["volume"]) + "'>" + text + "\" && aplay -D convertQBO /home/pi/Documents/pico2wave.wav"
 result = subprocess.call(speak, shell = True)
 time.sleep(0.5)
 
@@ -42,5 +45,7 @@ elif config["startWith"] == "interactive":
 
         result = subprocess.call(speak, shell = True)
 
-	result = subprocess.call("/home/pi/Documents/Python\ projects/PiFaceFast.py > /home/pi/PiFaceFastMode.log 2>&1 &", shell = True)
+	result = subprocess.call("/home/pi/Documents/deamonsScripts/QBO_PiFaceFast start > /home/pi/scratchMode.log", shell = True)
+
+#	result = subprocess.call("/home/pi/Documents/Python\ projects/PiFaceFast.py > /home/pi/PiFaceFastMode.log 2>&1 &", shell = True)
 
